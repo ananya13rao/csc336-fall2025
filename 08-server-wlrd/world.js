f70fcc7dd44fe6d4d24b23479df03dbe90c887bb
ananya13rao <ar9790a@american.edu>
2025-11-10 23:08:46 -0500

    push

--- 08-server-wlrd/world.js
+++
@@ -1,6 +0,0 @@
-async function getWorld() {
-let response = await fetch("/world");
-let world = await response.json(); 
-document.querySelector("body").innerHTML = `<h1>${world.regions[0].name}</h1>`
-
-}
---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+function App() {
+  
+  const [task, setTask] = useState(""); 
+
+  const [todos, setTodos] = useState([
+    {
+      task: "Do homework", 
+      complete: false,
+      id: 1
+    }, 
+    {
+      task: "Clean room", 
+      complete: true,
+      id: 2
+    },
+    {
+      task: "Make dinner",
+      complete: true,
+      id: 3
+    }
+  ]); 
+
+  function addTodo() {
+    let newTodo = {
+      task: task,
+      complete: false,
+      id: Date.now()
+    }
+
+    setTodos([...todos, newTodo]); 
+    setTask("");
+  }
+
+  return (
+    <div>
+
+      <input 
+        type = "text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+
+      <ul>
+        {}
+        {}
+        {}
+        {}
+        {}
+        {todos.map((todo) => (
+        <TodoItem todo={todo} key = {todo.id}/>
+        ))}
+
+      </ul>
+
+    </div>
+  )
+
+}
+export default App;
