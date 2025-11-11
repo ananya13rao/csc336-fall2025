bbfdb7e93e3729ff00fc7c80a8b86cd3e81e7b86
ananya13rao <ar9790a@american.edu>
2025-11-11 15:09:33 -0500

    push

--- 09-react-basics/src/App.jsx
+++ 09-react-basics/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [task, setTask] = useState(""); 
+
+  const [todos, setTodos] = useState([ // list stored in State
+    {
+      task: "Do homework", 
+      complete: false,
+      id: 1
+    }, 
+    {
+      task: "Clean room", 
+      complete: true,
+      id: 2
+    },
+    {
+      task: "Make dinner",
+      complete: true,
+      id: 3
+    }
+  ]); 
+
+  function addTodo() {
+    let newTodo = {
+      task: task,
+      complete: false,
+      id: Date.now()
+    }
+
+    setTodos([...todos, newTodo]); 
+    setTask("");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div> {/*ui*/}
 
-export default App
+      <input 
+        type = "text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+
+      <ul>
+        {}
+        {}
+        {}
+        {}
+        {}
+        {todos.map((todo) => (
+        <TodoItem todo={todo} key = {todo.id}/>
+        ))}
+
+      </ul>
+
+    </div>
+  )
+
+}
+export default App;
---
+++ 09-react-basics/src/TodoItem.jsx
@@ -0,0 +1,18 @@
+import "./TodoItem.css"
+
+
+function TodoItem({todo}) {
+    return (
+        <li
+        style={
+            {
+                textDecoration: todo.complete ? "line-through" : "none"
+            }
+        }>
+            {}
+            {todo.task}
+        </li>
+    )
+}
+
+export default TodoItem
